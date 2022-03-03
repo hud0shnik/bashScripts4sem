00b1c7876623ea9c30d687710db9738ee71abe17
Danila Egorov <c4nsero@mail.ru>
2022-03-03 18:58:56 +0300

    + script1-3

---
+++ lab2/script02.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# display user home
+echo "Home for the current user is: $HOME"
---
+++ lab2/script03.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# testing variables
+grade=5
+person="Adam"
+echo "$person is a good boy, he is in grade $grade"
