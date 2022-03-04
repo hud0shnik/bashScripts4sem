8568d88791cdaa8df82ef8e46b20642f17e4583f
Danila Egorov <c4nsero@mail.ru>
2022-03-04 11:27:42 +0300

    + scripts 28-31

---
+++ lab2/script29.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+total=$[ $1 + $2 ]
+echo The first parameter is $1.
+echo The second parameter is $2.
+echo The sum is $total.
---
+++ lab2/script31.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -n "$1" ]
+then
+echo Hello $1.
+else
+echo "No parameters found. "
+fi
